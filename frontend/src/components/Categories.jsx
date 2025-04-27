6bcf65c0cf5007e5b58cc7bc1dc39d796cdfc87f
Nakul Verma <workwithnakul09@gmail.com>
2025-04-27 13:17:55 +0530

    some changes

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -11,10 +11,10 @@ const Categories = () => {
     <div className="h-auto w-full flex flex-col justify-center items-center gap-6 p-6 sm:p-10">
       <h1 className="font-extrabold text-3xl sm:text-5xl text-center">Popular Categories</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-5xl place-items-center">
-        <Card src={design} title="Design" />
-        <Card src={advertising} title="Advertising" />
-        <Card src={film} title="Film" />
-        <Card src={photography} title="Photography" />
+        <Card src={design} title="Finance" />
+        <Card src={advertising} title="Technology" />
+        <Card src={film} title="Entertainment" />
+        <Card src={photography} title="Miscelleaneous" />
       </div>
       <Button Title="Explore" className="mt-6 sm:mt-10" />
     </div>
